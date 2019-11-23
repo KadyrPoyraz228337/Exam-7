67b0a6f5a686b4370209fcf045b3ec6a2393acc1
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-11-23 07:30:59 +0200

    Добавили BootStrap

--- src/components/AddItems/index.js
+++ src/components/AddItems/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./style.css";
+import Item from "../Item";
 
 const AddItems = () => {
     return (
         <div>
-            
+            <Item/>
         </div>
     );
 };
