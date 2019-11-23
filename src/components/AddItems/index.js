fac23bc18857c20a66058db82807079a9293691b
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-11-23 07:37:16 +0200

    Добавили функционал для Item

--- src/components/AddItems/index.js
+++ src/components/AddItems/index.js
@@ -5,7 +5,7 @@ import Item from "../Item";
 const AddItems = () => {
     return (
         <div>
-            <Item/>
+
         </div>
     );
 };
--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import "./style.css";
 
-const Item = () => {
+const Item = props => {
     return (
-        <div>
-
+        <div className="card">
+            <img src={props.logo} alt="Dish photo"/>
+            <div>
+                <h4>{props.title}</h4>
+                <p>Prise: {props.prise}</p>
+            </div>
         </div>
     );
 };
