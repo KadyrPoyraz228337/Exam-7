b424a1555ff25cf15c23832379e3efd30dd008a3
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-11-23 07:58:09 +0200

    Добавили константу dishPrise вне класса App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, {Component} from 'react';
 import OrderDetails from "./components/orderDetails";
 import AddItems from "./components/AddItems";
 
+const dishesPrise = [
+    {name: 'Hamburger', prise: 80, ico: 'https://image.flaticon.com/icons/png/512/26/26393.png'},
+    {name: 'Cheeseburger', prise: 90, ico: 'https://image.flaticon.com/icons/png/512/26/26393.png'},
+    {name: 'Fries', prise: 45, ico: 'https://image.flaticon.com/icons/png/512/26/26393.png'},
+    {name: 'Coffee', prise: 70, ico: 'https://www.clipartwiki.com/clipimg/detail/43-433867_big-paper-cup-drink-soda-water-svg-png.png'},
+    {name: 'Tea', prise: 50, ico: 'https://www.clipartwiki.com/clipimg/detail/43-433867_big-paper-cup-drink-soda-water-svg-png.png'},
+    {name: 'Cola', prise: 40, ico: 'https://www.clipartwiki.com/clipimg/detail/43-433867_big-paper-cup-drink-soda-water-svg-png.png'},
+];
+
 class App extends Component {
     state = {
         items: [
